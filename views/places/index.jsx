a2a58b75b58f172361cb0ec57a06f8d341f95833
milcab <milcarodriguez23@gmail.com>
2022-01-03 20:46:47 -0500

    rename files

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,18 +2,25 @@ const React = require("react");
 const Default = require("../layouts/default");
 
 function Index({ places }) {
-  const placesFormatted = places.map((place) => (
-    <div>
-      <h2>{place.name}</h2>
-      <img src={place.pic} alt={place.name} />
-    </div>
-  ));
+  let placesFormatted = places.map((place, index) => {
+    return (
+      <div className="col-sm-6 col-md-4">
+        <h2>
+          <a href={`/places/${index}`}>{place.name}</a>
+        </h2>
+        <p className="text-center">{place.cuisines}</p>
+        <img src={place.pic} alt={place.name} />
+        <p className="text-center">
+          Located in {place.city}, {place.state}
+        </p>
+      </div>
+    );
+  });
 
   return (
     <Default>
       <h1>Places Page</h1>
-
-      {placesFormatted}
+      <div className="row">{placesFormatted}</div>
     </Default>
   );
 }
