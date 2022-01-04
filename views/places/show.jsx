75156ce806040e0d8a1cbd68911020aa21648cf3
milcab <milcarodriguez23@gmail.com>
2022-01-03 21:11:39 -0500

    update views

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,28 @@
 const React = require("react");
-const Def = require("../default");
+const Def = require("../layouts/default");
 
-function show() {
+function show({ place }) {
   return (
     <Def>
       <main>
-        <h1>Show Page</h1>
+        <div className="row">
+          <div className="col-sm-12 col-md-6">
+            <img src={place.pic} alt={place.name} />
+          </div>
+          <div className="col-sm-12 col-md-6">
+            <h1>{place.name}</h1>
+            <p>Rating</p>
+            <p>not Rated</p>
+            <p>description</p>
+            <p>blah blah</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <h2>Comments</h2>
+            <p>no comments yet</p>
+          </div>
+        </div>
       </main>
     </Def>
   );
