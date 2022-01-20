349696413154867aae7a9fb8116f3e77589284c1
milcab <milcarodriguez23@gmail.com>
2022-01-20 18:32:56 -0500

    fixing the page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,40 +2,51 @@ const React = require("react");
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 const Def = require("../layouts/default");
 
-function show({ place, index }) {
+function show(data) {
   return (
     <Def>
       <main>
         <div className="row">
           <div className="col-sm-12 col-md-6 p-3">
-            <img src={place.pic} alt={place.name} />
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
           </div>
           <div className="col-sm-12 col-md-6 p-3">
             <div className="row p-3">
               <div className="col-sm-12 p-1">
-                <a href={`/places/${index}/edit`} className="btn btn-warning">
+                <a
+                  href={`/places/${data.place.id}/edit`}
+                  className="btn btn-warning"
+                >
                   Edit <FaEdit />
                 </a>
               </div>
               <div className="col-sm-12 p-1">
-                <form method="POST" action={`/places/${index}?_method=DELETE`}>
+                <form
+                  method="POST"
+                  action={`/places/${data.place.id}?_method=DELETE`}
+                >
                   <button type="submit" className="btn btn-danger">
                     Delete <FaTrashAlt />
                   </button>
                 </form>
               </div>
             </div>
-            <h1>{place.name}</h1>
-            <p>Rating</p>
-            <p>not Rated</p>
-            <p>description</p>
-            <p>blah blah</p>
+            <h1>{data.place.name}</h1>
+            <h2 className="color-change">Rating</h2>
+            <h3>Not Rated</h3>
+            <h2 className="color-change">Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>serving {data.place.cuisines}</h4>
           </div>
         </div>
+        
         <div className="row">
           <div className="col-sm-12 p-3">
-            <h2>Comments</h2>
-            <p>no comments yet</p>
+            <h2 className="color-change">Comments</h2>
+            <h4>No comments yet</h4>
           </div>
         </div>
       </main>
