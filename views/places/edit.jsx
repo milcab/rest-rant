c000c8c6e6e7baeb6a675a6131c21b1643acd5f0
milcab <milcarodriguez23@gmail.com>
2022-01-22 00:08:26 -0500

    update edit form

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -10,64 +10,84 @@ function Index({ place }) {
       <main>
         <h1>Edit Place</h1>
         <form action="edit?_method=PUT" method="POST">
-          <div className="form-group">
-            <label htmlFor="placeName">Place Name</label>
-            <input
-              required
-              type="text"
-              className="form-control"
-              id="placeName"
-              name="name"
-              value={place.name}
-              aria-describedby="placeHelp"
-              placeholder="Enter Place Name"
-            />
-
-            <label htmlFor="placeName">Place Picture</label>
-            <input
-              required
-              type="text"
-              className="form-control"
-              id="placeName"
-              name="picture"
-              value={place.pic}
-              aria-describedby="placeHelp"
-              placeholder="Enter Picture"
-            />
-            <label htmlFor="placeName">City</label>
-            <input
-              type="text"
-              className="form-control"
-              id="placeName"
-              name="city"
-              value={place.city}
-              aria-describedby="placeHelp"
-              placeholder="Enter City"
-            />
-            <label htmlFor="placeName">State</label>
-            <input
-              type="text"
-              className="form-control"
-              id="placeName"
-              name="state"
-              value={place.state}
-              aria-describedby="placeHelp"
-              placeholder="Enter State Name"
-            />
-            <label htmlFor="placeName">Cuisines</label>
-            <input
-              type="text"
-              className="form-control"
-              id="placeName"
-              name="cuisines"
-              value={place.cuisines}
-              aria-describedby="placeHelp"
-              placeholder="Cuisines Name :-)"
-            />
-            <small id="placeHelp" className="form-text text-muted">
-              We love place!
-            </small>
+          <div className="container my-4">
+            <div className="row">
+              <div className="form-group col-sm-4">
+                <label htmlFor="placeName">Place Name</label>
+                <input
+                  required
+                  type="text"
+                  className="form-control"
+                  id="placeName"
+                  name="name"
+                  value={place.name}
+                  aria-describedby="placeHelp"
+                  placeholder="Enter Place Name"
+                />
+              </div>
+              <div className="form-group col-sm-4">
+                <label htmlFor="placeName">Place Picture</label>
+                <input
+                  required
+                  type="text"
+                  className="form-control"
+                  id="placeName"
+                  name="picture"
+                  value={place.pic}
+                  aria-describedby="placeHelp"
+                  placeholder="Enter Picture"
+                />
+              </div>
+              <div className="form-group col-sm-4">
+                <label htmlFor="placeName">City</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="placeName"
+                  name="city"
+                  value={place.city}
+                  aria-describedby="placeHelp"
+                  placeholder="Enter City"
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="form-group col-sm-4">
+                <label htmlFor="placeName">State</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="placeName"
+                  name="state"
+                  value={place.state}
+                  aria-describedby="placeHelp"
+                  placeholder="Enter State Name"
+                />
+              </div>
+              <div className="form-group col-sm-4">
+                <label htmlFor="placeName">Cuisines</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="placeName"
+                  name="cuisines"
+                  value={place.cuisines}
+                  aria-describedby="placeHelp"
+                  placeholder="Cuisines Name :-)"
+                />
+              </div>
+              <div className="form-group col-sm-4">
+                <label htmlFor="founded">Founded</label>
+                <input
+                  className="form-control"
+                  id="founded"
+                  name="founded"
+                  value={place.founded}
+                />
+              </div>
+            </div>
           </div>
+
           <button type="submit" className="btn btn-primary">
             Submit
           </button>
